b9f14f310a7a91eb7eab78e4c6075b0a75c2feae
vominhtan <vominhtan1988@gmail.com>
2022-09-14 18:49:36 +0700

    3 files modified

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ console.log(`Hello Node.js v${process.versions.node}!`);
 const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
   fs.writeFileSync(
     `${fileName}.json`,
     JSON.stringify(vCardParser.parse(data).map(toPatient), null, 4)
